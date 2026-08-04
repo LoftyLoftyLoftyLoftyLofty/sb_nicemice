eee33943a09b3fe006be7f1e4827eb471d1a5592
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2026-08-04 04:48:20 -0500

    whip progress 3

--- scripts/actions/nicemice_whipaim.lua
+++ scripts/actions/nicemice_whipaim.lua
@@ -11,3 +11,17 @@ function nicemice_entityCenterPosition(args, board)
   local center = {(boundBox[1] + boundBox[3]) / 2, (boundBox[2] + boundBox[4]) / 2}
   return true, {position = center}
 end
+
+-- param number
+-- output result
+function nicemice_signOf(args, board)
+  if args.number == nil then return false end
+
+  if args.number > 0 then
+    return true, {result = 1}
+  elseif args.number < 0 then
+    return true, {result = -1}
+  else
+    return true, {result = 0}
+  end
+end
