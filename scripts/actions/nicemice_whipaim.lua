54026c4cb72e5693d5e813bbec511a82d06d30a9
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2026-08-05 14:45:01 -0500

    whips are working properly now

--- scripts/actions/nicemice_whipaim.lua
+++ scripts/actions/nicemice_whipaim.lua
@@ -2,14 +2,9 @@
 -- output position
 function nicemice_entityCenterPosition(args, board)
   if args.entity == nil or not world.entityExists(args.entity) then return false end
-
-  local boundBox = world.entityMetaBoundBox(args.entity)
-  if boundBox == nil then
-    return true, {position = world.entityPosition(args.entity)}
-  end
-
-  local center = {(boundBox[1] + boundBox[3]) / 2, (boundBox[2] + boundBox[4]) / 2}
-  return true, {position = center}
+	local p = world.entityPosition(args.entity)
+	--sb.logInfo( "entity: " .. sb.printJson( args.entity ) )
+    return true, {position = p, x = p[1], y = p[2]}
 end
 
 -- param number
@@ -24,4 +19,21 @@ function nicemice_signOf(args, board)
   else
     return true, {result = 0}
   end
+end
+
+-- param from
+-- param to
+-- output vector
+-- output x
+-- output y
+-- output magnitude
+function nicemice_entityDistance(args, board)
+  -- sb.logInfo( "dist check: " .. sb.printJson( args.fromEntity ) .. " -> " .. sb.printJson( args.toEntity ) )
+  if args.fromEntity == nil or args.toEntity == nil then return false end
+  
+  local from = world.entityPosition(args.fromEntity)
+  local to = world.entityPosition(args.toEntity)
+  
+  local distance = world.distance(to, from)
+  return true, {vector = distance, x = distance[1], y = distance[2], magnitude = world.magnitude(to, from)}
 end
